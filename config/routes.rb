d580bf43537c379d0040aad100a2d5b45f238bbd
emanuel caro <darkemlord@gmail.com>
2022-10-23 15:13:03 +0900

    refs #001 setting server

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace 'v1' do
+  namespace :v1 do
     resources :drinks
   end
   # Defines the root path route ("/")
