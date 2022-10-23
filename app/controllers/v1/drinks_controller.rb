8106e331b201e539f9c2165e62cb6bd75c9236fa
emanuel caro <darkemlord@gmail.com>
2022-10-23 19:19:27 +0900

    refs #001 authentication flow working

--- app/controllers/v1/drinks_controller.rb
+++ app/controllers/v1/drinks_controller.rb
@@ -1,6 +1,7 @@
 class V1::DrinksController < ApplicationController
+  before_action :authenticate_user!
   def index
     @drinks = Drink.all
-    render json: @drinks
+    render json: @drinks, status: :ok
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_23_093536) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -34,8 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_23_093536) do
     t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
-    t.index ["reset_password_token"],
-            name: "index_users_on_reset_password_token",
-            unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
